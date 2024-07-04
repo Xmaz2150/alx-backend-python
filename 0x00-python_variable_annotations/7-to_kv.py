ba1dfb6796647f6dd1903f378fd09313a3165cd7
tAf <muzirat0@gmail.com>
2024-07-05 01:45:33 +0200

    type lists

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 """
-sum_mixed_list annotated module
+to_kv annotated module
 """
-from typing import List, Union
+from typing import Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
