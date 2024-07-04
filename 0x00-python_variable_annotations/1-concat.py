be37d2963e176c322d65610b578d0e32a4abab10
tAf <muzirat0@gmail.com>
2024-07-05 01:04:06 +0200

    concats strs

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+concat annotated module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concats str1 and str2, returns as str
+    """
+    return str1 + str2
