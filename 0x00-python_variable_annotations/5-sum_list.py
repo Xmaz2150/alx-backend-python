931468ac7176c6c51e5eb0a8b9de26e9e2dd47d0
tAf <muzirat0@gmail.com>
2024-07-05 01:24:42 +0200

    type lists

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+sum_list annotated module
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns input_list sum as float
+    """
+    return sum(input_list)
