68af3d733d234de5b2cc58307a8668cbb2dbc834
tAf <muzirat0@gmail.com>
2024-07-28 22:58:18 +0200

    mock decorated method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -46,7 +46,7 @@ class TestGetJson(unittest.TestCase):
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_get_json):
         """
-        mocks `get json` 
+        mocks `get json`
         """
         mock_response = Mock()
         mock_response.json.return_value = test_payload
@@ -54,5 +54,37 @@ class TestGetJson(unittest.TestCase):
 
         res = get_json(test_url)
 
-        mock_get_json.asser_called_once_with(test_url)
+        mock_get_json.assert_called_once_with(test_url)
         self.assertEqual(res, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    memoize test class
+    """
+    def test_memoize(self):
+        """
+        mocks a method decorated by `memoize`
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) \
+                as mock_a_method:
+            """
+            alternative to patch decoration on `test_memoize`
+            """
+            test = TestClass()
+
+            res1 = test.a_property
+            res2 = test.a_property
+
+            mock_a_method.assert_called_once()
+            self.assertEqual(res1, 42)
+            self.assertEqual(res2, 42)
