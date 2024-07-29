d55fc77250adc8c0806e4af617060cb78dc41bcb
tAf <muzirat0@gmail.com>
2024-07-29 21:28:11 +0200

    method test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -56,3 +56,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(res['repos_url'], 'https://api.github.com/orgs/google/repos')
 
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """
+        GithubOrgClient.has_license test case
+        """
+        res = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(res, expected)
